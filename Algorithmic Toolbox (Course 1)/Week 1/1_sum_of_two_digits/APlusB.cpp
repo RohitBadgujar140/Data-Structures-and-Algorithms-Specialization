340598f6c7602858924e2a769129e0c7366afa5f
Rohit Badgujar <65911824+RohitBadgujar48@users.noreply.github.com>
2021-07-19 20:24:44 +0530

    Update APlusB.cpp

--- Algorithmic Toolbox (Course 1)/Week 1/1_sum_of_two_digits/APlusB.cpp
+++ Algorithmic Toolbox (Course 1)/Week 1/1_sum_of_two_digits/APlusB.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int sum_of_two_digits(int first_digit, int second_digit) {
     return first_digit + second_digit;
 }
 
-int main() {
+int main(int argc, char **argv) {
+
     int a = 0;
     int b = 0;
-    std::cin >> a;
-    std::cin >> b;
-    std::cout << sum_of_two_digits(a, b);
+    cin>>a;
+    cin>>b;
+    cout<<sum_of_two_digits(a, b);
     return 0;
 }
